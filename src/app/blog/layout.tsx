f0ddd3aea26701c5bfce6f0ab589a111cf194224
lennartnm <office@ln-ecommerce.de>
2025-08-27 15:22:27 +0200

    Update layout.tsx

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
+import Script from "next/script";
 
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-white">
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-E00ZMCKQYL"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-E00ZMCKQYL');
+        `}
+      </Script>
+
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
