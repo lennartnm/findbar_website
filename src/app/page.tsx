20cddc4e8f2cb11e5dc1b2f87c5b2c405f25ecfb
lennartnm <office@ln-ecommerce.de>
2025-09-01 18:06:30 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -699,7 +699,7 @@ function HowItHelpsSection() {
               }}
             />
             <img
-              src="/Lupe.png"
+              src="/LupeIcon.png"
               alt="Darstellung von KI-optimierten Blogartikeln"
               className="relative z-20 w-full rounded-sm"
             />
@@ -894,7 +894,7 @@ function UnserZielSection() {
                   }}
                 />
                 <img
-                  src="/Rakete.png"
+                  src="/RaketeIcon.png"
                   alt="Darstellung des Done 4 You Prozesses"
                   className="relative z-20 w-full rounded-sm"
                 />
