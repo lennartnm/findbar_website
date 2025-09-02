cfbb4cc493b3aeb5ed96e30cd6408161bd7beff6
lennartnm <office@ln-ecommerce.de>
2025-09-02 11:40:24 +0200

    Update page.tsx

--- src/app/funktionsweise/page.tsx
+++ src/app/funktionsweise/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/ui/Header";
+import Footer from "@/components/ui/Footer";
 import styles from "./funktionsweise.module.css";
 
 export const metadata: Metadata = {
